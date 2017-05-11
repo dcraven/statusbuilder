b96371e656b1ba2445478403985e678dcd2a226e
Dennis Craven <dcraven@gmail.com>
2017-05-11 15:43:13 -0400

    Add getStatusItemIdList to Report class.

--- backend/src/main/java/com/dcraven/model/Report.java
+++ backend/src/main/java/com/dcraven/model/Report.java
@@ -1,9 +1,15 @@
 package com.dcraven.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Report {
-    List<StatusItem> statusItemList;
+    List<StatusItem> statusItemList = new ArrayList<>();
+
+    public List<Long> getStatusItemIdList(Long id) {
+        return statusItemList.stream().map(statusItem -> statusItem.getId()).collect(Collectors.toList());
+    }
 
     public List<StatusItem> getStatusItemList() {
         return statusItemList;
